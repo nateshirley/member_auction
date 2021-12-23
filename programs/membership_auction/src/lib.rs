3eb95572464e9cb51c9ca8079362d1b63d7ef3a0
Nathan Shirley <nshirley22@gmail.com>
2021-12-22 19:13:17 -0500

    big auction but it's actually good

--- programs/membership_auction/src/lib.rs
+++ programs/membership_auction/src/lib.rs
@@ -5,7 +5,8 @@ declare_id!("6Zt81sekecE5npwcDSLveAH1Uvs75DfXqaquLbXdPpD1");
 const HOUSE_SEED: &[u8] = b"house_auth";
 const MEMBERSHIP_AUCTION_SEED: &[u8] = b"mship_axn";
 const WINNERS_SEED: &[u8] = b"winners";
-const NUM_BIDS: usize = 4;
+const HAS_CLAIMED_SEED: &[u8] = b"has_claimed";
+const NUM_BIDS: usize = 20;
 const MINIMUM_OPENING_BID: u64 = 100;
 
 mod anchor_transfer;
@@ -20,31 +21,45 @@ pub mod membership_auction {
     /*
     1. create auction
     2. place bid
-    3. finalize auction
     4. claim winnings
+    */
+
+    /*
+    ok so say i wanted to shard the bids
+    i would basically run someth client-side that detects which auction has the lowest running bid?
+    and then i would submit a bid to that auction
+    i could try to prevent the bidder from submitting across multiple different auctions
+    but i'm not sure why i would really need to do that
+    i don't even think i need to do anything else
+
+    if i put a timer on it i wouldn't even need to keep the winners in here
+    verify clock u feel
+
     */
 
     pub fn create_membership_auction(
         ctx: Context<CreateMembershipAuction>,
         membership_auction_bump: u8,
+        has_claimed_bump: u8,
         epoch: u32,
+        index: u8,
     ) -> ProgramResult {
         //obvi need more checks around when it can start
 
-        ctx.accounts.membership_auction.bump = membership_auction_bump;
         ctx.accounts.membership_auction.epoch = epoch;
         ctx.accounts.membership_auction.start_timestamp =
             u64::try_from(ctx.accounts.clock.unix_timestamp).unwrap();
+        ctx.accounts.membership_auction.index = index;
+        ctx.accounts.membership_auction.bump = membership_auction_bump;
+        ctx.accounts.membership_auction.bids = [Bid::default(); NUM_BIDS];
 
-        ctx.accounts.membership_auction.bids = [Bid::default(); 4];
+        ctx.accounts.has_claimed.bump = has_claimed_bump;
         Ok(())
     }
 
-    //lol i need to work on the leaderboard, this is much cleaner.
     pub fn place_bid(
         ctx: Context<PlaceBid>,
         house_authority_bump: u8,
-        _epoch: u32,
         amount: u64,
     ) -> ProgramResult {
         //storing bids in descending order so i can always pop the lowest value off the end
@@ -61,6 +76,7 @@ pub mod membership_auction {
         };
         //turn it back on later
         //verify_unique_bidder(ctx.accounts.bidder.key, &open_bids)?;
+        //p sure if there's no match it returns where the value should be
         match open_bids.binary_search_by(|probe| probe.cmp(&new_bid).reverse()) {
             Ok(pos) => {
                 //someone submits a bid that is equal to an existing bid, but higher than the minimum
@@ -91,29 +107,13 @@ pub mod membership_auction {
         Ok(())
     }
 
-    pub fn settle_membership_auction(
-        ctx: Context<SettleMembershipAuction>,
-        _winners_bump: u8,
-        _epoch: u32,
-    ) -> ProgramResult {
-        //validate the time
-        //declare the winners
-
-        for (i, winning_bid) in ctx.accounts.membership_auction.bids.iter().enumerate() {
-            ctx.accounts.winners.record[i] = MembershipAuctionWinner {
-                wallet: winning_bid.bidder,
-                has_claimed: false,
-            };
-        }
-        Ok(())
-    }
-
     pub fn claim_membership_from_auction(
         ctx: Context<ClaimMembershipFromAuction>,
     ) -> ProgramResult {
-        for winner in ctx.accounts.winners.record.iter_mut() {
-            if winner.wallet.eq(ctx.accounts.claimant.key) && !winner.has_claimed {
-                winner.has_claimed = true;
+        let bids = ctx.accounts.membership_auction.bids;
+        for (i, has_claimed) in ctx.accounts.has_claimed.record.iter_mut().enumerate() {
+            if bids[i].bidder.eq(ctx.accounts.claimant.key) && !*has_claimed {
+                *has_claimed = true;
                 return Ok(());
                 //mint the new membership
             }
@@ -165,6 +165,102 @@ fn return_lamps_to_newest_loser(
     Ok(())
 }
 
+#[derive(Accounts)]
+#[instruction(membership_auction_bump: u8, has_claimed_bump: u8, epoch: u32, index: u8)]
+pub struct CreateMembershipAuction<'info> {
+    creator: Signer<'info>,
+    #[account(
+        init,
+        seeds = [MEMBERSHIP_AUCTION_SEED, &epoch.to_le_bytes(), &[index]],
+        bump = membership_auction_bump,
+        payer = creator
+    )]
+    membership_auction: Box<Account<'info, MembershipAuction>>,
+    #[account(
+        init,
+        seeds = [HAS_CLAIMED_SEED, &epoch.to_le_bytes(), &[index]],
+        bump = has_claimed_bump,
+        payer = creator
+    )]
+    has_claimed: Box<Account<'info, HasClaimed>>,
+    clock: Sysvar<'info, Clock>,
+    system_program: Program<'info, System>,
+}
+
+//add check to verify Membership auction time
+#[derive(Accounts)]
+#[instruction(house_authority_bump: u8)]
+pub struct PlaceBid<'info> {
+    #[account(mut)]
+    bidder: Signer<'info>,
+    #[account(mut)]
+    newest_loser: AccountInfo<'info>,
+    #[account(
+        mut,
+        seeds = [MEMBERSHIP_AUCTION_SEED, &membership_auction.epoch.to_le_bytes(), &[membership_auction.index]],
+        bump = membership_auction.bump,
+    )]
+    membership_auction: Account<'info, MembershipAuction>,
+    #[account(
+        mut,
+        seeds = [HOUSE_SEED],
+        bump = house_authority_bump
+    )]
+    house_authority: UncheckedAccount<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct MembershipAuction {
+    epoch: u32,
+    start_timestamp: u64,
+    bids: [Bid; 20],
+    index: u8,
+    bump: u8,
+}
+
+#[derive(Accounts)]
+pub struct ClaimMembershipFromAuction<'info> {
+    claimant: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [MEMBERSHIP_AUCTION_SEED, &membership_auction.epoch.to_le_bytes(), &[membership_auction.index]],
+        bump = membership_auction.bump,
+    )]
+    membership_auction: Account<'info, MembershipAuction>,
+    #[account(
+        mut,
+        seeds = [HAS_CLAIMED_SEED, &membership_auction.epoch.to_le_bytes(), &[membership_auction.index]],
+        bump = has_claimed.bump,
+    )]
+    has_claimed: Account<'info, HasClaimed>,
+}
+
+#[account]
+pub struct HasClaimed {
+    record: [bool; 20],
+    bump: u8,
+}
+
+impl Default for HasClaimed {
+    fn default() -> HasClaimed {
+        HasClaimed {
+            record: [false; 20],
+            bump: 0,
+        }
+    }
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("bid does not meet minimum for this auction (lowest * 1.02)")]
+    LowBallBid,
+    #[msg("bidding wallet has already placed a bid on this auction")]
+    NonUniqueBidder,
+    #[msg("wallet has no authority to claim. either they didn't win or they already claimed")]
+    NoAuctionClaimAuthority,
+}
 fn verify_unique_bidder(new_bidder: &Pubkey, open_bids: &Vec<Bid>) -> ProgramResult {
     for bid in open_bids {
         if bid.bidder.eq(new_bidder) {
@@ -205,44 +301,19 @@ fn verify_bid_amount(
     }
 }
 
-#[derive(Accounts)]
-#[instruction(membership_auction_bump: u8, epoch: u32)]
-pub struct CreateMembershipAuction<'info> {
-    creator: Signer<'info>,
-    #[account(
-        init,
-        seeds = [MEMBERSHIP_AUCTION_SEED, &epoch.to_le_bytes()],
-        bump = membership_auction_bump,
-        payer = creator
-    )]
-    membership_auction: Account<'info, MembershipAuction>,
-    clock: Sysvar<'info, Clock>,
-    system_program: Program<'info, System>,
+/*
+
+#[account]
+#[derive(Default)]
+pub struct MembershipAuctionWinners {
+    record: [MembershipAuctionWinner; 20],
 }
 
-//add check to verify Membership auction time
-#[derive(Accounts)]
-#[instruction(house_authority_bump: u8, epoch: u32)]
-pub struct PlaceBid<'info> {
-    #[account(mut)]
-    bidder: Signer<'info>,
-    #[account(mut)]
-    newest_loser: AccountInfo<'info>,
-    #[account(
-        mut,
-        seeds = [MEMBERSHIP_AUCTION_SEED, &epoch.to_le_bytes()],
-        bump = membership_auction.bump,
-    )]
-    membership_auction: Account<'info, MembershipAuction>,
-    #[account(
-        mut,
-        seeds = [HOUSE_SEED],
-        bump = house_authority_bump
-    )]
-    house_authority: UncheckedAccount<'info>,
-    system_program: Program<'info, System>,
+#[derive(Default, Clone, Copy, AnchorDeserialize, AnchorSerialize)]
+pub struct MembershipAuctionWinner {
+    wallet: Pubkey,
+    has_claimed: bool,
 }
-
 #[derive(Accounts)]
 #[instruction(winners_bump: u8, epoch: u32)]
 pub struct SettleMembershipAuction<'info> {
@@ -254,49 +325,11 @@ pub struct SettleMembershipAuction<'info> {
     membership_auction: Account<'info, MembershipAuction>,
     #[account(
         init,
-        seeds = [WINNERS_SEED, &epoch.to_le_bytes()],
+        seeds = [HAS_CLAIMED_SEED, &epoch.to_le_bytes(), &[membership_auction.index]],
         bump = winners_bump,
         payer = settler
     )]
-    winners: Account<'info, MembershipAuctionWinners>,
+    has_claimed: Account<'info, HasClaimed>,
     system_program: Program<'info, System>,
 }
-
-#[derive(Accounts)]
-pub struct ClaimMembershipFromAuction<'info> {
-    claimant: Signer<'info>,
-    //add address verification
-    #[account(mut)]
-    winners: Account<'info, MembershipAuctionWinners>,
-}
-
-#[account]
-#[derive(Default)]
-pub struct MembershipAuctionWinners {
-    record: [MembershipAuctionWinner; 4],
-}
-
-#[derive(Default, Clone, Copy, AnchorDeserialize, AnchorSerialize)]
-pub struct MembershipAuctionWinner {
-    wallet: Pubkey,
-    has_claimed: bool,
-}
-
-#[account]
-#[derive(Default)]
-pub struct MembershipAuction {
-    epoch: u32,
-    start_timestamp: u64,
-    bids: [Bid; 4],
-    bump: u8,
-}
-
-#[error]
-pub enum ErrorCode {
-    #[msg("bid does not meet minimum for this auction (lowest * 1.02)")]
-    LowBallBid,
-    #[msg("bidding wallet has already placed a bid on this auction")]
-    NonUniqueBidder,
-    #[msg("wallet has no authority to claim. either they didn't win or they already claimed")]
-    NoAuctionClaimAuthority,
-}
+*/
--- tests/membership_auction.ts
+++ tests/membership_auction.ts
@@ -16,8 +16,10 @@ describe("membership_auction", () => {
   let houseAuthority = null;
   let houseAuthorityBump = null;
   let epoch = 9;
-  let membershipAuction = null;
-  let membershipAuctionBump = null;
+  let auctionAddresses: PublicKey[] = [];
+  let auctionBumps: number[] = [];
+  let hasClaimedAddresses: PublicKey[] = [];
+  let hasClaimedBumps: number[] = [];
   let bidder = Keypair.generate();
   let otherBidder = Keypair.generate();
   let winners = null;
@@ -36,9 +38,15 @@ describe("membership_auction", () => {
     // console.log("ARRRR: ", arr);
     houseAuthority = _house;
     houseAuthorityBump = _houseBump;
-    let [_mA, _mBump] = await getMembershipAuctionAddress(epoch);
-    membershipAuction = _mA;
-    membershipAuctionBump = _mBump;
+
+    for (let i = 0; i < 4; i++) {
+      let [_mA, _mBump] = await getMembershipAuctionAddress(epoch, i);
+      auctionAddresses[i] = _mA;
+      auctionBumps[i] = _mBump;
+      let [_has, _hasBump] = await getHasClaimedAddress(epoch, i);
+      hasClaimedAddresses[i] = _has;
+      hasClaimedBumps[i] = _hasBump;
+    }
 
     await provider.connection.requestAirdrop(
       bidder.publicKey,
@@ -57,176 +65,132 @@ describe("membership_auction", () => {
 
   it("create membership auction", async () => {
     // Add your test here.
+    let index = 0;
     const tx = await program.rpc.createMembershipAuction(
-      membershipAuctionBump,
+      auctionBumps[index],
+      hasClaimedBumps[index],
       epoch,
+      index,
       {
         accounts: {
           creator: creator.publicKey,
-          membershipAuction: membershipAuction,
+          membershipAuction: auctionAddresses[index],
+          hasClaimed: hasClaimedAddresses[index],
           clock: web3.SYSVAR_CLOCK_PUBKEY,
           systemProgram: web3.SystemProgram.programId,
         },
+        instructions: createAuctionInstructions(1, 3),
       }
     );
-    console.log("Your transaction signature", tx);
+    //console.log("Your transaction signature", tx);
+    let hasC = await program.account.hasClaimed.fetch(hasClaimedAddresses[0]);
+    console.log(hasC);
   });
+
+  const createAuctionInstructions = (
+    startingIndex: number,
+    endIndex: number
+  ) => {
+    let instructions: anchor.web3.TransactionInstruction[] = [];
+    for (let i = startingIndex; i <= endIndex; i++) {
+      instructions.push(
+        program.instruction.createMembershipAuction(
+          auctionBumps[i],
+          hasClaimedBumps[i],
+          epoch,
+          i,
+          {
+            accounts: {
+              creator: creator.publicKey,
+              membershipAuction: auctionAddresses[i],
+              hasClaimed: hasClaimedAddresses[i],
+              clock: web3.SYSVAR_CLOCK_PUBKEY,
+              systemProgram: web3.SystemProgram.programId,
+            },
+          }
+        )
+      );
+    }
+    return instructions;
+  };
+
+  // const caIx = (i: number) => {
+
+  // }
 
   it("place a bid", async () => {
     let preBid = await provider.connection.getBalance(creator.publicKey);
     //console.log("preBid: ", preBid);
 
-    let amount = 1 * web3.LAMPORTS_PER_SOL;
-    let amountBN = new BN(amount);
+    await placeBid(2, 0);
+    await placeBid(2, 1);
+    await placeBid(2, 2);
+    await placeBid(2, 3);
 
-    const tx = await program.rpc.placeBid(houseAuthorityBump, epoch, amountBN, {
-      accounts: {
-        bidder: bidder.publicKey,
-        newestLoser: await getLosingBidder(membershipAuction),
-        membershipAuction: membershipAuction,
-        houseAuthority: houseAuthority,
-        systemProgram: web3.SystemProgram.programId,
-      },
-      signers: [bidder],
-    });
+    let houseBalance = await provider.connection.getBalance(houseAuthority);
+    console.log("house: ", houseBalance);
 
-    await program.rpc.placeBid(
-      houseAuthorityBump,
-      epoch,
-      new BN(2 * web3.LAMPORTS_PER_SOL),
-      {
-        accounts: {
-          bidder: bidder.publicKey,
-          newestLoser: await getLosingBidder(membershipAuction),
-          membershipAuction: membershipAuction,
-          houseAuthority: houseAuthority,
-          systemProgram: web3.SystemProgram.programId,
-        },
-        signers: [bidder],
-      }
+    let bids = await program.account.membershipAuction.fetch(
+      auctionAddresses[1]
     );
-
-    await program.rpc.placeBid(
-      houseAuthorityBump,
-      epoch,
-      new BN(0.5 * web3.LAMPORTS_PER_SOL),
-      {
-        accounts: {
-          bidder: bidder.publicKey,
-          newestLoser: await getLosingBidder(membershipAuction),
-          membershipAuction: membershipAuction,
-          houseAuthority: houseAuthority,
-          systemProgram: web3.SystemProgram.programId,
-        },
-        signers: [bidder],
-      }
-    );
-
-    await program.rpc.placeBid(
-      houseAuthorityBump,
-      epoch,
-      new BN(1.8 * web3.LAMPORTS_PER_SOL),
-      {
-        accounts: {
-          bidder: otherBidder.publicKey,
-          newestLoser: await getLosingBidder(membershipAuction),
-          membershipAuction: membershipAuction,
-          houseAuthority: houseAuthority,
-          systemProgram: web3.SystemProgram.programId,
-        },
-        signers: [otherBidder],
-      }
-    );
-
-    await program.rpc.placeBid(
-      houseAuthorityBump,
-      epoch,
-      new BN(1 * web3.LAMPORTS_PER_SOL),
-      {
-        accounts: {
-          bidder: bidder.publicKey,
-          newestLoser: await getLosingBidder(membershipAuction),
-          membershipAuction: membershipAuction,
-          houseAuthority: houseAuthority,
-          systemProgram: web3.SystemProgram.programId,
-        },
-        signers: [bidder],
-      }
-    );
-
-    await program.rpc.placeBid(
-      houseAuthorityBump,
-      epoch,
-      new BN(1.5 * web3.LAMPORTS_PER_SOL),
-      {
-        accounts: {
-          bidder: otherBidder.publicKey,
-          newestLoser: await getLosingBidder(membershipAuction),
-          membershipAuction: membershipAuction,
-          houseAuthority: houseAuthority,
-          systemProgram: web3.SystemProgram.programId,
-        },
-        signers: [otherBidder],
-      }
-    );
-
-    let balance = await provider.connection.getBalance(bidder.publicKey);
-    console.log(balance);
-
-    let postBid = await provider.connection.getBalance(creator.publicKey);
-    //console.log("postBid: ", postBid);
-
-    let bids = await program.account.membershipAuction.fetch(membershipAuction);
     let storedBids: any = bids.bids;
-    storedBids.map((bid) => {
-      let bidder: PublicKey = bid.bidder;
-      console.log("bidder: ", bidder.toBase58());
-      console.log("amount: ", bid.amount.toNumber());
-    });
+    // storedBids.map((bid) => {
+    //   let bidder: PublicKey = bid.bidder;
+    //   console.log("bidder: ", bidder.toBase58());
+    //   console.log("amount: ", bid.amount.toNumber());
+    // });
   });
 
-  it("settle auction", async () => {
-    const tx = await program.rpc.settleMembershipAuction(winnersBump, epoch, {
-      accounts: {
-        settler: creator.publicKey,
-        membershipAuction: membershipAuction,
-        winners: winners,
-        systemProgram: web3.SystemProgram.programId,
-      },
-    });
-
-    let results = await program.account.membershipAuctionWinners.fetch(winners);
-    let record: any = results.record;
-    record.map((recordedWinner) => {
-      let winner: PublicKey = recordedWinner.wallet;
-      console.log("bidder: ", winner.toBase58());
-      console.log("hasclaimed: ", recordedWinner.hasClaimed);
-    });
-  });
+  const placeBid = async (sol: number, index: number) => {
+    await program.rpc.placeBid(
+      houseAuthorityBump,
+      new BN(sol * web3.LAMPORTS_PER_SOL),
+      {
+        accounts: {
+          bidder: bidder.publicKey,
+          newestLoser: await getLosingBidder(auctionAddresses[index]),
+          membershipAuction: auctionAddresses[index],
+          houseAuthority: houseAuthority,
+          systemProgram: web3.SystemProgram.programId,
+        },
+        signers: [bidder],
+      }
+    );
+  };
 
   it("claim memberhsip from auction", async () => {
     const tx = await program.rpc.claimMembershipFromAuction({
       accounts: {
-        claimant: otherBidder.publicKey,
-        winners: winners,
+        claimant: bidder.publicKey,
+        membershipAuction: auctionAddresses[0],
+        hasClaimed: hasClaimedAddresses[0],
       },
-      signers: [otherBidder],
+      signers: [bidder],
     });
 
-    let results = await program.account.membershipAuctionWinners.fetch(winners);
-    let record: any = results.record;
-    record.map((recordedWinner) => {
-      let winner: PublicKey = recordedWinner.wallet;
-      console.log("bidder: ", winner.toBase58());
-      console.log("hasclaimed: ", recordedWinner.hasClaimed);
-    });
+    let results = await program.account.hasClaimed.fetch(
+      hasClaimedAddresses[0]
+    );
+    console.log(results);
   });
+  /*
+   */
 
-  const getMembershipAuctionAddress = async (epoch: number) => {
-    let toArrayLike = new Int32Array([epoch]).buffer;
-    let epochArray = new Uint8Array(toArrayLike);
+  const getMembershipAuctionAddress = async (epoch: number, index: number) => {
+    let toEpochArrayLike = new Int32Array([epoch]).buffer;
+    let epochArray = new Uint8Array(toEpochArrayLike);
+    let indexArray = new Uint8Array([index]);
     return await PublicKey.findProgramAddress(
-      [anchor.utils.bytes.utf8.encode("mship_axn"), epochArray], //
+      [anchor.utils.bytes.utf8.encode("mship_axn"), epochArray, indexArray], //
+      program.programId
+    );
+  };
+  const getHasClaimedAddress = async (epoch: number, index: number) => {
+    let toEpochArrayLike = new Int32Array([epoch]).buffer;
+    let epochArray = new Uint8Array(toEpochArrayLike);
+    let indexArray = new Uint8Array([index]);
+    return await PublicKey.findProgramAddress(
+      [anchor.utils.bytes.utf8.encode("has_claimed"), epochArray, indexArray], //
       program.programId
     );
   };
@@ -247,3 +211,72 @@ describe("membership_auction", () => {
     );
   };
 });
+
+/*
+it("settle auction", async () => {
+    const tx = await program.rpc.settleMembershipAuction(winnersBump, epoch, {
+      accounts: {
+        settler: creator.publicKey,
+        membershipAuction: auctionAddresses[0],
+        winners: winners,
+        systemProgram: web3.SystemProgram.programId,
+      },
+    });
+
+    let results = await program.account.membershipAuctionWinners.fetch(winners);
+    let record: any = results.record;
+    record.map((recordedWinner) => {
+      let winner: PublicKey = recordedWinner.wallet;
+      console.log("bidder: ", winner.toBase58());
+      console.log("hasclaimed: ", recordedWinner.hasClaimed);
+    });
+  });
+*/
+
+// await program.rpc.placeBid(
+//   houseAuthorityBump,
+//   epoch,
+//   new BN(1.8 * web3.LAMPORTS_PER_SOL),
+//   {
+//     accounts: {
+//       bidder: otherBidder.publicKey,
+//       newestLoser: await getLosingBidder(membershipAuction),
+//       membershipAuction: membershipAuction,
+//       houseAuthority: houseAuthority,
+//       systemProgram: web3.SystemProgram.programId,
+//     },
+//     signers: [otherBidder],
+//   }
+// );
+
+// await program.rpc.placeBid(
+//   houseAuthorityBump,
+//   epoch,
+//   new BN(1 * web3.LAMPORTS_PER_SOL),
+//   {
+//     accounts: {
+//       bidder: bidder.publicKey,
+//       newestLoser: await getLosingBidder(membershipAuction),
+//       membershipAuction: membershipAuction,
+//       houseAuthority: houseAuthority,
+//       systemProgram: web3.SystemProgram.programId,
+//     },
+//     signers: [bidder],
+//   }
+// );
+
+// await program.rpc.placeBid(
+//   houseAuthorityBump,
+//   epoch,
+//   new BN(1.5 * web3.LAMPORTS_PER_SOL),
+//   {
+//     accounts: {
+//       bidder: otherBidder.publicKey,
+//       newestLoser: await getLosingBidder(membershipAuction),
+//       membershipAuction: membershipAuction,
+//       houseAuthority: houseAuthority,
+//       systemProgram: web3.SystemProgram.programId,
+//     },
+//     signers: [otherBidder],
+//   }
+// );
